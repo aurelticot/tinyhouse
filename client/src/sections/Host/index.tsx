fc8b6c324085939e0a72ca1076ec3386cf3c75a2
Aurel <aurelticot.dev@gmail.com>
2020-09-26 12:31:35 +1000

    implement the listing creation form

--- client/src/sections/Host/index.tsx
+++ client/src/sections/Host/index.tsx
@@ -1,5 +1,166 @@
-import React from "react";
+import React, { useState } from "react";
+import { Layout, Typography, Form, Input, InputNumber, Radio, Upload, Button } from "antd";
+import { HomeOutlined, BankOutlined, PlusOutlined, LoadingOutlined } from "@ant-design/icons";
+import { Viewer } from "../../lib/types";
+import { Link } from "react-router-dom";
+import { ListingType } from "../../lib/graphql/globalTypes";
+import { iconColor, displayErrorMessage } from "../../lib/utils";
+import { UploadChangeParam } from "antd/lib/upload";
 
-export const Host = () => {
-  return <h2>Host</h2>;
+const { Content } = Layout;
+const { Text, Title } = Typography;
+const { Item } = Form;
+
+interface Props {
+  viewer: Viewer;
+}
+
+export const Host = ({ viewer }: Props) => {
+  const [imageLoading, setImageLoading] = useState(false);
+  const [imageBase64Value, setImageBase64Value] = useState<string | null>(null);
+
+  const handleImageUpload = (info: UploadChangeParam) => {
+    const { file } = info;
+
+    if (file.status === "uploading") {
+      setImageLoading(true);
+      return;
+    }
+
+    if (file.status === "done" && file.originFileObj) {
+      getbase64Value(file.originFileObj, (value) => {
+        setImageBase64Value(value);
+        setImageLoading(false);
+      });
+    }
+  };
+
+  if (!viewer.id || !viewer.hasWallet) {
+    return (
+      <Content className="host-content">
+        <div className="host__form-header">
+          <Title level={3} className="host__form-title">
+            You'll have to be signed in and connected to Stripe to host a listing!
+          </Title>
+          <Text type="secondary">
+            We only allow users who've signed in to our application and have connected with Stripe to host new listings.
+            You can sing in on the <Link to="/login">Login page</Link> and connect to Strip shortly after.
+          </Text>
+        </div>
+      </Content>
+    );
+  }
+
+  return (
+    <Content className="host-content">
+      <Form layout="vertical">
+        <div className="host__form-header">
+          <Title level={3} className="host__form-title">
+            Hi! Let's get started listing your place
+          </Title>
+          <Text type="secondary">
+            In this form, we'll collect some basic and additional information about your listing
+          </Text>
+        </div>
+
+        <Item label="Home Type">
+          <Radio.Group>
+            <Radio.Button value={ListingType.APARTMENT}>
+              <HomeOutlined style={{ color: iconColor }} /> <span>Apartment</span>
+            </Radio.Button>
+            <Radio.Button value={ListingType.HOUSE}>
+              <BankOutlined style={{ color: iconColor }} /> <span>House</span>
+            </Radio.Button>
+          </Radio.Group>
+        </Item>
+
+        <Item label="Title" extra="Max character count of 400">
+          <Input maxLength={45} placeholder="The iconic and luxurious Bel-Air mansion" />
+        </Item>
+
+        <Item label="Description of listing" extra="Max character count of 45">
+          <Input.TextArea
+            rows={3}
+            maxLength={400}
+            placeholder="Modern, clean and iconic home of the Fresh Prince. Situated in the heart Bel-Air, Los Angeles"
+          />
+        </Item>
+
+        <Item label="Address">
+          <Input placeholder="251 North Bristol Avenue" />
+        </Item>
+
+        <Item label="City/Town">
+          <Input placeholder="Los Angeles" />
+        </Item>
+
+        <Item label="State/Province">
+          <Input placeholder="California" />
+        </Item>
+
+        <Item label="Zip/Postal Code">
+          <Input placeholder="Please enter a zip code for your listing" />
+        </Item>
+
+        <Item label="Image" extra="Images have to be under 1MB in size and of type JPG or PNG">
+          <div className="host__form-image-upload">
+            <Upload
+              name="image"
+              listType="picture-card"
+              showUploadList={false}
+              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+              beforeUpload={beforeImageUpload}
+              onChange={handleImageUpload}
+            >
+              {imageBase64Value ? (
+                <img src={imageBase64Value} alt="Listing image" />
+              ) : (
+                <div>
+                  {imageLoading ? <LoadingOutlined /> : <PlusOutlined />}
+                  <div className="ant-upload-text">Upload</div>
+                </div>
+              )}
+            </Upload>
+          </div>
+        </Item>
+
+        <Item label="Price" extra="Price in $USD/day">
+          <InputNumber min={0} placeholder="120" />
+        </Item>
+
+        <Item label="Number of guests" extra="">
+          <InputNumber min={0} placeholder="4" />
+        </Item>
+
+        <Item>
+          <Button type="primary">Submit</Button>
+        </Item>
+      </Form>
+    </Content>
+  );
+};
+
+const beforeImageUpload = (file: File) => {
+  const fileIsValidImage = file.type === "image/jpeg" || file.type === "image/png";
+  const fileIsValidSize = file.size / 1024 / 1024 < 1;
+
+  if (!fileIsValidImage) {
+    displayErrorMessage("You're only able to upload valid JPG or PNG files!");
+    return false;
+  }
+
+  if (!fileIsValidSize) {
+    displayErrorMessage("You're only able to upload valid image files of under 1MB in size!");
+    return false;
+  }
+
+  return fileIsValidImage && fileIsValidImage;
+};
+
+const getbase64Value = (img: File | Blob, callback: (value: string) => void) => {
+  const reader = new FileReader();
+  reader.readAsDataURL(img);
+  reader.onload = () => {
+    callback(reader.result as string);
+  };
 };
